de8db386e80c3f65c33c11572932f1be801a3954
LaithSarraf <laith.sarraf@gmail.com>
2021-06-08 23:12:43 +0300

    added JS codes

--- Js/app.js
+++ Js/app.js
@@ -1,65 +1,108 @@
-'use strick';
+'use strict';
 
-let userName = prompt('Please Introduce yourself')
-alert(`Hello ` +  userName + `, welcome to my personal website, it is time to know me`)
+// let userName = prompt('Please Introduce yourself')
+// alert(`Hello ` +  userName + `, welcome to my personal website, it is time to know me`)
 
-let myCoffee = prompt('Do i love Coffee with sugar?').toLowerCase();
-    if(myCoffee === 'yes' || myCoffee === 'y') {
-    alert('Correct! You know how i like my Coffee!');
-}
-//console.log(myCoffee)
+// let myCoffee = prompt('Do i love Coffee with sugar?').toLowerCase();
+//     if(myCoffee === 'yes' || myCoffee === 'y') {
+//     alert('Correct! You know how i like my Coffee!');
+// }
+// //console.log(correct)
 
-else if (myCoffee === 'no' || myCoffee === 'n') {
-    alert('Oh No! I can not drink coffee with sugar!');
-}   else{
-    alert('Please type (Yes) or (Y) or (No) or (N)');
-}
+// else if (myCoffee === 'no' || myCoffee === 'n') {
+//     alert('Oh No! I can not drink coffee with sugar!');
+// }   else{
+//     alert('Please type (Yes) or (Y) or (No) or (N)');
+// }
 
 
 
-let favSport = prompt('Since you know how I drink my Coffee, is Boxing my favorite sport?').toLowerCase();
-    if(favSport === 'yes' || favSport === 'y') {
-        alert('Good Job my Friend! you know my favorite sport');
-    } 
-    //console.log(favSport)
+// let favSport = prompt('Since you know how I drink my Coffee, is Boxing my favorite sport?').toLowerCase();
+//     if(favSport === 'yes' || favSport === 'y') {
+//         alert('Good Job my Friend! you know my favorite sport');
+//     } 
+//     //console.log(correct)
 
-    else if (favSport === 'no' || favSport === 'n'){
-        alert('It is ok, Boxing is my favorite sport!:)');
-    } else {
-            alert('Please type (Yes) or (Y) or (No) or (N)');
-        }
+//     else if (favSport === 'no' || favSport === 'n'){
+//         alert('It is ok, Boxing is my favorite sport!:)');
+//     } else {
+//             alert('Please type (Yes) or (Y) or (No) or (N)');
+//         }
         
     
-    let favCar = prompt('Since you know my favorite sport, is Porsche my favorite car?').toLowerCase();
-    //console.log(favCar)
+//     let favCar = prompt('Since you know my favorite sport, is Porsche my favorite car?').toLowerCase();
+//     //console.log(correct)
 
-        if (favCar === 'yes' || favCar === 'y') {
-            alert('Nice!! You know my favorite car now!')
-        }
-        else if (favCar === 'no' || favCar === 'n') {
-            alert('Ooops! Porsche is my favorite car')
+//         if (favCar === 'yes' || favCar === 'y') {
+//             alert('Nice!! You know my favorite car now!')
+//         }
+//         else if (favCar === 'no' || favCar === 'n') {
+//             alert('Ooops! Porsche is my favorite car')
             
-        } else {
-            alert('Please type (Yes) or (Y) or (No) or (N)');
-        }
+//         } else {
+//             alert('Please type (Yes) or (Y) or (No) or (N)');
+//         }
   
-        let myAge = prompt('Now it is time to know my age, was I born in 1991?').toLowerCase
-        //console.log(myAge)
-        if (myAge === 'yes' || myAge === 'y') {
-            alert('Good guess, am I older than you?');
-        }
+//         let myAge = prompt('Now it is time to know my age, was I born in 1991?').toLowerCase
+//         //console.log(correct)
+//         if (myAge === 'yes' || myAge === 'y') {
+//             alert('Good guess, am I older than you?');
+//         }
         
 
-        else if (myAge === 'no' || myAge === 'n'){
-            alert('Tough luck! Yes I was born in 1991!');
-        }
+//         else if (myAge === 'no' || myAge === 'n'){
+//             alert('Tough luck! Yes I was born in 1991!');
+//         }
 
-        else {
-            alert('Please type (Yes) or (Y) or (No) or (N)');
-        }
+//         else {
+//             alert('Please type (Yes) or (Y) or (No) or (N)');
+//         }
 
-        alert(`Hello ` +  userName + `, now you know me a little bit better, see you soon`)
-        git
+//         alert(`Hello ` +  userName + `, now you know me a little bit better, see you soon`)
+        // git
+
+      
+        // while(favNumber < 7 ) {
+        //     favNumber = prompt('Too COLD, raise the number!')
+        // } while(favNumber > 7 ){
+        //     favNumber = prompt('Too HOT, Lower the number!')
+        // }
+
+
+        // for(let i = 0; i < 4; i++){
+        //     let favNumber = prompt('What is my favorite number?')
+
+        //     if (favNumber < 7 ){
+        //         alert('Too Cold!');
+        //     }
+        //     else if (favNumber > 7 ){
+        //         alert('Too Hot!!');
+        //     }
+        //     else { 
+        //         alert('Correct, you got the number right')
+        //         break
+        //     }
+        //   if (i === 3){
+        //       alert('The correct is 7, tough luck')
+        //   }
+        // }
+        
+        // let score = 0
+        
+        let boxingChamp = ['Mike Tyson', 'Michael Spinks', 'Bone Crusher Smith', 'Trevor Berbick','Laith Sarraf', 'Floyd Money Mayweather', 'Evander Holyfield', 'Anthoney Joshua', 'Vasyl Lomachenko', 'George Foreman'];
+let boxer = prompt('Which boxer won a championship in the 1980s')
+        
+        for(let i = 0; i < 6; i++){
+            if (boxer === boxingChamp[i])
+break; 
+        }
+        
+        
+// while (boxingChamp === 'Mike Tyson' || boxingChamp === 'Michael Spinks' || boxingChamp === 'Trevor Berbick')
+//         
+
+
+
 
   //  let yearBorn = prompt('Now it is time to know my age, was I born in 1991?').toLowerCase
     // if (yearBorn === 'yes' || yearBorn === 'y') {
